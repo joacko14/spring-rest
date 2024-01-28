f0f8e7d9e991828ce3f87de4970a1e7b64eb111a
Joaquin Vazquez <joacko14@gmail.com>
2024-01-27 18:28:10 -0600

    First step creating a model, a repository and a controller for CRUD actions with REST api

--- src/main/java/com/learning/rest/controllers/PersonController.java
+++ src/main/java/com/learning/rest/controllers/PersonController.java
@@ -2,7 +2,6 @@ package com.learning.rest.controllers;
 
 import com.learning.rest.interfaces.PersonInterface;
 import com.learning.rest.models.Person;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
